8491e9aa0596d7bfc61b6384d7c3ebbabf48b705
LTito03~ <ltitonorabuena@gmail.com>
2025-04-04 19:40:10 -0500

    archivos imprimir e ingreso

---
+++ Program.py
@@ -0,0 +1,15 @@
+
+a = 2**3 + 1
+
+if a == 8:
+    print(a)
+else:
+    print("no")
+
+b = "Hola" " " "amigo"
+
+print(b)
+
+c = "Estudia " * 5
+print(c)
+
---
+++ imprimir.py
@@ -0,0 +1,38 @@
+a = "Carlos"
+b = 20
+print("Hola" , a , "tienes " , b)
+
+
+# comentario una linea
+
+"""
+comentario varias lineas
+"""
+
+print("Carlos","Jose","Juan")
+print("Carlos","Jose","Juan", sep="* ", end="%\n") #sep= controla los espacios   "%\n" salto de linea
+print("Carlos","Jose","Juan")
+
+
+d = f"Hola {a} tienes {b}"
+print(d)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
